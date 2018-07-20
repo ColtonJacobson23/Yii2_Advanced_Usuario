2c7cd84a8d4576b4a800bcbcfca8ef7f72a6082f
ColtonJacobson23 <coltonjjacobson@gmail.com>
2018-07-20 11:33:45 -0700

    Yii2 Advanced Template is working with 2amigos/Usuario module.

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -14,7 +14,16 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'cj.yii.test@gmail.com',
+                'password' => 'gCnFaBh@ck3rs',
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],
+
         ],
     ],
 ];
